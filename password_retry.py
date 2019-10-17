3a5bc30e1054e9c62eb733b3f3df2a40c13d638c
unknown <tony31318921@gmail.com>
2019-10-18 01:26:26 +0800

    basic version

---
+++ password_retry.py
@@ -0,0 +1,17 @@
+# 密碼重複程式
+# password = 'a123'
+# 最多輸入三次
+# 如果正確印出 "登入成功"
+# 如果不正確 印出"密碼錯誤! 還有_次機會!"
+password = 'a123'
+i = 3
+while True:
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤! 還有', i,'次機會!')
+		if i == 0:
+			break
